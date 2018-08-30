c172ac44f05f0f283110928e09dd9e3cb420ed76
Lucas Menéndez <epucas@gmail.com>
2018-08-30 20:51:55 +0200

    build setup and initial README.md

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 <p align="center">
-    <img src="assets/images/logo.svg" width="450">
+    <img src="assets/images/logo.svg" width="400">
 </p>
+
+# WebProletarian
+Simple JavaScript framework to execute inline function into a WebWorker easily.
+
+<br/>
+<br/>
+
+## Installation
+
+### From Source Code
+1. Clone or download zipped source code into `node_modules` project folder.
+```sh
+cd <project>/node_modules 
+git clone https://github.com/lucasmenendez/webproletarian.git
+```
+
+2. Get into the library folder and install required dependencies.
+```sh
+cd webproletarian && npm install
+```
+
+3. Build library
+```sh
+npm run build
+```
+
+
+## Using WebProletarian
+Import minified version into the `index.html` file:
+
+```html
+<script src="/node_modules/webproletarian/dist/webproletarian.js>
+```
+
+Or import with ES6 `import`:
+```javascript
+import WebProletarian from 'webproletarian'
+```
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const ROOTPATH = "..";
+const PROD = process.env.NODE_ENV === 'production';
 
 function resolve(dir) {
 	return path.resolve(__dirname, ROOTPATH, dir);
@@ -25,20 +27,26 @@ module.exports = {
                 loader: 'babel-loader'
 			}
 		]
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({ 
+				uglifyOptions: {
+					compress: PROD === true,
+					keep_classnames: PROD === true,
+					mangle: PROD === true
+				}
+			})
+		]
 	}
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (PROD) {
 	module.exports.plugins = (module.exports.plugins || []).concat([
 		new webpack.DefinePlugin({
 			'process.env': {
 				NODE_ENV: '"production"'
 			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: { screw_ie8: true, warnings: false },
-			output: { comments: false },
-			sourceMap: false,
 		})
-	])
+	]);
 }
